0d1280e5d38eaea180d5441620d2aa66635ba331
deidmon <paul.dedios@alum.udep.edu.pe>
2023-10-09 22:56:33 -0500

    Cambiar splash

--- app_valtx_asistencia/lib/app/ui/views/splash/widgets/splash_content.dart
+++ app_valtx_asistencia/lib/app/ui/views/splash/widgets/splash_content.dart
@@ -1,3 +1,4 @@
+import 'package:app_valtx_asistencia/app/ui/views/login/login_view.dart';
 import 'package:app_valtx_asistencia/core/theme/app_colors.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
@@ -9,15 +10,18 @@ class SplashContent extends StatelessWidget {
   Widget build(BuildContext context) {
     return Container(
       padding: const EdgeInsets.symmetric(horizontal: 35.0),
-      decoration: const BoxDecoration(gradient: AppColors.primaryGradient),
+      decoration: const BoxDecoration(
+          color: AppColors.light /* gradient: AppColors.primaryGradient */),
       child: Center(
-        /* child: Image(
+          child: text(
+              "VALTX", 46.0, Color.fromARGB(255, 1, 2, 96), TextAlign.center)
+          /* child: Image(
           image: const AssetImage(
             "assets/images/logo_white.png",
           ),
           width: 220.w,
         ), */
-      ),
+          ),
     );
   }
 }
